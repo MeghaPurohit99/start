a2783d88b509895161cdbce218c9c519e08c0c65
Megha <meghapurohit711999@gmail.com>
2022-01-31 16:05:08 +0530

    second

---
+++ arithmetic.java
@@ -0,0 +1,13 @@
+class arithmetic
+{
+	public static void main(String args[])
+	{
+		int a=1,b=2,c=0,d=0,m=0;
+		c=a+b;
+        d = b-a;
+        m = a*b;
+		System.out.println(c);
+        System.out.println(d);
+        System.out.println(m);
+	}
+}
