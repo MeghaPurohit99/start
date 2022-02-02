18f2b692bfca4de9e906418687f13a2f68a88550
Megha <meghapurohit711999@gmail.com>
2022-02-02 11:37:49 +0530

    ignored file

--- arithmetic.java
+++ arithmetic.java
@@ -3,9 +3,9 @@ class arithmetic
 	public static void main(String args[])
 	{
 
-		int a=5980,b=22,c=0,d=0,m=0;
+		int a=98,b=22,c=0,d=0,m=0;
 		c=a+b;
-        d = b-a;
+        d = a-b;
         m = a*b;
 		System.out.println("addition is : "+c);
         System.out.println("difference is : "+d);
